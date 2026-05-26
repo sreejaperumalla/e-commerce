069f7ee3f05d06bc2d107bc966bbcbe36716321f
Sreeja <perumallasreeja.04@gmail.com>
2026-05-26 12:15:30 +0530

    fix neon users table

--- server.js
+++ server.js
@@ -524,8 +524,6 @@ app.get("/admin/analytics", async (req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-
 });
